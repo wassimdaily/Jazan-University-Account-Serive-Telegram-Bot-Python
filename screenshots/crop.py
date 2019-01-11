a01fa5a60123c4c7bf776bba76dd5b331348a4fd
wassimdaily <32506057+wassimdaily@users.noreply.github.com>
2019-01-11 11:41:47 +0300

    Update crop.py

--- screenshots/crop.py
+++ screenshots/crop.py
@@ -7,5 +7,3 @@ area = (60, 20, 930, 610)
 crop = img.crop(area)
 
 crop.show()
-
-https://api.telegram.org/bot741491798:AAGAVtjhldPVKyjkPPN0qicXo3hYDfpjVFw/setWebhook?url=https://jazanu.000webhostapp.com/bd.php
